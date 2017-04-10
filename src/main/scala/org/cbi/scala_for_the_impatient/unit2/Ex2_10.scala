b7fe084422657fa135fb78535681171b916dbb85
yykhomenko <yykhomenko@gmail.com>
2017-04-10 19:22:13 +0300

    - add unit1 ex6

--- src/main/scala/org/cbi/scala_for_the_impatient/unit2/Ex2_10.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit2/Ex2_10.scala
@@ -2,11 +2,11 @@ package org.cbi.scala_for_the_impatient.unit2
 
 object Ex2_10 {
 
-  def ex_2_10_mypow(x: Double, n: Int): Double = {
-    if (n > 0 && n % 2 == 0) math.pow(ex_2_10_mypow(x, n / 2), 2)
-    else if (n > 0 && n % 2 == 1) x * ex_2_10_mypow(x, n - 1)
+  def mypow(x: Double, n: Int): Double = {
+    if (n > 0 && n % 2 == 0) math.pow(mypow(x, n / 2), 2)
+    else if (n > 0 && n % 2 == 1) x * mypow(x, n - 1)
     else if (n == 0) 1
-    else if (n < 0) 1 / ex_2_10_mypow(x, -n)
+    else if (n < 0) 1 / mypow(x, -n)
     else 1
   }
 }
--- src/main/scala/org/cbi/scala_for_the_impatient/unit2/Ex3.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit2/Ex3.scala
@@ -1,8 +1,8 @@
 package org.cbi.scala_for_the_impatient.unit2
 
-object Ex2_3 {
+object Ex3 {
 
-  def ex_2_3() {
+  def fun() {
     var x: Unit = None
     var y = 1
     x = y = 1
