477747c44a4ce5e5bd652f999aea46f9982a5484
yykhomenko <yykhomenko@gmail.com>
2017-04-10 20:19:41 +0300

    - some refactoring

--- src/main/scala/org/cbi/scala_for_the_impatient/unit2/Ex9.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit2/Ex9.scala
@@ -1,5 +1,5 @@
 package org.cbi.scala_for_the_impatient.unit2
 
-object Ex2_9 {
+object Ex9 {
   def ex_2_9_product(s: String): Long = if (s.length == 0) 1 else s.head.toInt * ex_2_9_product(s.tail)
 }
--- src/test/scala/org/cbi/scala_for_the_impatient/unit2/Ex10Test.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit2/Ex10Test.scala
@@ -1,9 +1,9 @@
 package org.cbi.scala_for_the_impatient.unit2
 
-import org.cbi.scala_for_the_impatient.unit2.Ex2_10.mypow
+import org.cbi.scala_for_the_impatient.unit2.Ex10.mypow
 import org.specs2.mutable.Specification
 
-class Ex2_10Test extends Specification {
+class Ex10Test extends Specification {
 
   "Ex2_10Test" should {
     "ex_2_10_mypow return pow equal" in { mypow(2.4, 5) mustEqual 79.62624 }
