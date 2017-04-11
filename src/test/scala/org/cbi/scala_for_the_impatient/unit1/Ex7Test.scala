15258d734c70d31f93826ec2610b3a195af1b854
yykhomenko <yykhomenko@gmail.com>
2017-04-11 22:48:58 +0300

    - corrected test message U1 Ex7

--- src/test/scala/org/cbi/scala_for_the_impatient/unit1/Ex7Test.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit1/Ex7Test.scala
@@ -4,6 +4,6 @@ import org.specs2.mutable.Specification
 
 class Ex7Test extends Specification {
   "U1 Ex7Test" should {
-    "The value BigInt should contain 100 bit" in { Ex7.value.bitLength mustEqual 100 }
+    "value should contains 100 bit" in { Ex7.value.bitLength mustEqual 100 }
   }
 }
