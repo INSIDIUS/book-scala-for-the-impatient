da53589ab341e8222e9da522242f0f8b636d9ef6
y.khomenko <insidius89@gmail.com>
2017-04-11 22:47:13 +0300

    - corrected test message U2 Ex10 - deleted one test in U2 Ex10

--- src/test/scala/org/cbi/scala_for_the_impatient/unit2/Ex10Test.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit2/Ex10Test.scala
@@ -4,9 +4,7 @@ import org.cbi.scala_for_the_impatient.unit2.Ex10.mypow
 import org.specs2.mutable.Specification
 
 class Ex10Test extends Specification {
-
   "U2 Ex10Test" should {
-    "mypow return pow equal" in { mypow(2.4, 5) mustEqual 79.62624 }
-    "mypow return pow not equal" in { mypow(2.4, 5) mustNotEqual 79.62625 }
+    "mypow(2.4, 5) return value equal" in { mypow(2.4, 5) mustEqual 79.62624 }
   }
 }
