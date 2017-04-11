c7b85432ac17e820762a1b9e1995da9791efa0b7
yykhomenko <yykhomenko@gmail.com>
2017-04-11 22:40:50 +0300

    - refactored U3 Ex2 - added test

---
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit3/Ex2.scala
@@ -0,0 +1,16 @@
+package org.cbi.scala_for_the_impatient.unit3
+
+object Ex2 {
+
+  def arr() = {
+    val arr = Array(1 to 5 : _*)
+    for (i <- arr.indices by 2 if i < arr.length - 1) swap(arr, i, i + 1)
+    arr
+  }
+
+  private def swap(arr: Array[Int], first: Int, second: Int) = {
+    val tmp = arr(first)
+    arr(first) = arr(second)
+    arr(second) = tmp
+  }
+}
---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit3/Ex2Test.scala
@@ -0,0 +1,9 @@
+package org.cbi.scala_for_the_impatient.unit3
+
+import org.specs2.mutable.Specification
+
+class Ex2Test extends Specification {
+  "Ex2Test" should {
+    "returned array equal" in { Ex2.arr() mustEqual Array(2, 1, 4, 3, 5)}
+  }
+}
