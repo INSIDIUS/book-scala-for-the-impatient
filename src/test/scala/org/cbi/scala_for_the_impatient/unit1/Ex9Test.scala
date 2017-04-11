573cc6086baaecc5211e725e32afaf5927d3d055
yykhomenko <yykhomenko@gmail.com>
2017-04-11 22:50:02 +0300

    - corrected test message U1 Ex9

--- src/test/scala/org/cbi/scala_for_the_impatient/unit1/Ex9Test.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit1/Ex9Test.scala
@@ -4,7 +4,7 @@ import org.specs2.mutable.Specification
 
 class Ex9Test extends Specification {
   "U1 Ex9Test" should {
-    "The 'Hello' string should have a first char is 'H'" in { Ex9.firstChar mustEqual 'H' }
-    "The 'Hello' string should have a last char is 'o'" in { Ex9.lastChar mustEqual 'o' }
+    "'Hello' string have a first char is 'H'" in { Ex9.firstChar mustEqual 'H' }
+    "'Hello' string have a last char is 'o'" in { Ex9.lastChar mustEqual 'o' }
   }
 }
