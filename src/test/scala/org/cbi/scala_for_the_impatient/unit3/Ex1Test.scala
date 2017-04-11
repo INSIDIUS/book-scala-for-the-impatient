5075892f29c1c5633b6b61b86c6ef4f63d58c371
y.khomenko <insidius89@gmail.com>
2017-04-11 22:41:44 +0300

    - correct test message

--- src/test/scala/org/cbi/scala_for_the_impatient/unit3/Ex1Test.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit3/Ex1Test.scala
@@ -4,6 +4,6 @@ import org.specs2.mutable.Specification
 
 class Ex1Test extends Specification {
   "U3 Ex1Test" should {
-    "return array value of arrayFrom0to(10) must contains 10 elements" in { Ex1.arrayFrom0to(10).length mustEqual 10}
+    "returned array contains 10 elements" in { Ex1.arrayFrom0to(10).length mustEqual 10}
   }
 }
