8b832c1c601e9b5e424796b93a986cdd2e986eff
y.khomenko <insidius89@gmail.com>
2017-04-11 22:45:31 +0300

    - del space U2 Ex9

--- src/test/scala/org/cbi/scala_for_the_impatient/unit2/Ex9Test.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit2/Ex9Test.scala
@@ -4,7 +4,6 @@ import org.cbi.scala_for_the_impatient.unit2.Ex9.product
 import org.specs2.mutable.Specification
 
 class Ex9Test extends Specification {
-
   "U2 Ex9Test" should {
     "product(\"Hello\") return equal" in { product("Hello") mustEqual 9415087488L }
   }
