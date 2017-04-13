5ea8a628274230144ffa380b8e0a6b2e55fcaa0f
yykhomenko <yykhomenko@gmail.com>
2017-04-13 21:31:42 +0300

    - add u3 ex3

---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit3/Ex3Test.scala
@@ -0,0 +1,10 @@
+package org.cbi.scala_for_the_impatient.unit3
+
+import org.specs2.mutable.Specification
+
+object Ex3Test extends Specification {
+
+  "Ex3Test" should {
+    "returned array equal" in { Ex3.arr() mustEqual Array(2, 1, 4, 3, 5)}
+  }
+}
