fb63371ced8aaa4412fbc4ad11351f62e996c87e
yykhomenko <yykhomenko@gmail.com>
2017-04-13 21:33:58 +0300

    - refactored

--- src/main/scala/org/cbi/scala_for_the_impatient/unit3/Ex3.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit3/Ex3.scala
@@ -3,7 +3,7 @@ package org.cbi.scala_for_the_impatient.unit3
 object Ex3 extends App {
 
   def arr() = {
-    val arr = Array(1 to 5 :_*)
+    val arr = Array(1 to 5: _*)
     val arrResult = for (i <- arr.indices if i < arr.length) yield {
       if (i % 2 == 0 && i == arr.length - 1) arr(i)
       else if (i == 0 || i % 2 == 0) arr(i + 1)
--- src/main/scala/org/cbi/scala_for_the_impatient/unit3/Ex3_4.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit3/Ex3_4.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable.ArrayBuffer
 object Ex3_4 extends App {
 
   def ex_3_4_arr() {
-    val arr = Array(-2 to 5 :_*)
+    val arr = Array(-2 to 5: _*)
     val arrResult = ArrayBuffer[Int]()
 
     arrResult ++= arr.filter(_ > 0)
