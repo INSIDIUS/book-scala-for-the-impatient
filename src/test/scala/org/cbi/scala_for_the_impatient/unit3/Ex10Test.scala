76e123467d52b05b35ac9c6e5ecaea2b369428ea
y.khomenko <insidius89@gmail.com>
2017-04-14 00:14:32 +0300

    - cleaned

--- src/test/scala/org/cbi/scala_for_the_impatient/unit3/Ex10Test.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit3/Ex10Test.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable.ArrayBuffer
 object Ex10Test extends Specification {
 
   "Ex10Test" should {
-    "flavors is ArrayBuffer" in { Ex10.flavors.getClass mustEqual ArrayBuffer[String]("").getClass }
+    "flavors is ArrayBuffer" in { Ex10.flavors.getClass mustEqual ArrayBuffer[String]().getClass }
     "flavors is not empty" in { Ex10.flavors.isEmpty mustEqual false }
   }
 }
