1741fae23e8a18199b733cb0fb47845896211694
yykhomenko <yykhomenko@gmail.com>
2017-04-14 00:13:44 +0300

    - add u3 ex10

---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit3/Ex10Test.scala
@@ -0,0 +1,13 @@
+package org.cbi.scala_for_the_impatient.unit3
+
+import org.specs2.mutable.Specification
+
+import scala.collection.mutable.ArrayBuffer
+
+object Ex10Test extends Specification {
+
+  "Ex10Test" should {
+    "flavors is ArrayBuffer" in { Ex10.flavors.getClass mustEqual ArrayBuffer[String]("").getClass }
+    "flavors is not empty" in { Ex10.flavors.isEmpty mustEqual false }
+  }
+}
