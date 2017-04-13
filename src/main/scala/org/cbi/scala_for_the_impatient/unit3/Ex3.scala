4867ce9e333a1cfb962c93728b9b56fb43edac25
y.khomenko <insidius89@gmail.com>
2017-04-13 21:31:42 +0300

    - add u3 ex3

--- src/main/scala/org/cbi/scala_for_the_impatient/unit3/Ex3.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit3/Ex3.scala
@@ -1,16 +1,14 @@
 package org.cbi.scala_for_the_impatient.unit3
 
-object Ex3_3 extends App {
+object Ex3 extends App {
 
-  def ex_3_3_arr() {
+  def arr() = {
     val arr = Array(1 to 5 :_*)
     val arrResult = for (i <- arr.indices if i < arr.length) yield {
       if (i % 2 == 0 && i == arr.length - 1) arr(i)
       else if (i == 0 || i % 2 == 0) arr(i + 1)
       else arr(i - 1)
     }
-    println(arrResult)
+    arrResult.toArray
   }
-
-  ex_3_3_arr()
 }
