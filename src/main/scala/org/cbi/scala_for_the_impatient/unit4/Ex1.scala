bf53312cecc26a6abaf2123d23e1761036699144
yykhomenko <yykhomenko@gmail.com>
2017-04-15 20:56:45 +0300

    - add u4 ex1

--- src/main/scala/org/cbi/scala_for_the_impatient/unit4/Ex1.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit4/Ex1.scala
@@ -1,13 +1,6 @@
 package org.cbi.scala_for_the_impatient.unit4
 
-object Ex4_1 extends App {
-
-  def ex_4_1() {
+object Ex1 {
     val components = Map("Monitor 4k" -> 12220, "Videocard Display Port" -> 4000)
     val componentsWithDiscount = for ((k, v) <- components) yield (k, v * 0.9)
-
-    println(componentsWithDiscount)
-  }
-
-  ex_4_1()
 }
