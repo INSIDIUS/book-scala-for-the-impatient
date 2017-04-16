4accd8cbe9c54a2f1d88687f3686b78ee867d984
yykhomenko <yykhomenko@gmail.com>
2017-04-16 12:46:30 +0300

    - added u4 ex4 one test

--- src/test/scala/org/cbi/scala_for_the_impatient/unit4/Ex4Test.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit4/Ex4Test.scala
@@ -5,6 +5,7 @@ import org.specs2.mutable.Specification
 object Ex4Test extends Specification {
 
   "Ex4Test" should {
+    "word \'Scala\' entres 8 times" in { Ex3.entries.getOrElse("Scala", 0) mustEqual 8 }
     "entries sorted by ASC" in { Ex4.entries equals Ex4.entries.toStream.sorted.toMap }
   }
 }
