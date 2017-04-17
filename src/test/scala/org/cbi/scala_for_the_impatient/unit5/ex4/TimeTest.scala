69200ede37820446b71b01977da2913a1d6f2177
y.khomenko <insidius89@gmail.com>
2017-04-17 16:38:02 +0300

    - added u5 ex4

---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit5/ex4/TimeTest.scala
@@ -0,0 +1,12 @@
+package org.cbi.scala_for_the_impatient.unit5.ex4
+
+import org.specs2.mutable.Specification
+
+object TimeTest extends Specification {
+
+  "U5 Ex4 TimeTest" should {
+    "past before future " in {
+      val past = new Time(22, 57); val future = new Time(23, 59); past.before(future) mustEqual true
+    }
+  }
+}
