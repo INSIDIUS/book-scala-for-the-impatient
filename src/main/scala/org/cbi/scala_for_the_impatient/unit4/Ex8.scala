d3f5b134ac5927ea4a2ea27706d367e5352ffe84
y.khomenko <insidius89@gmail.com>
2017-04-17 11:49:19 +0300

    - added u4 ex8

---
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit4/Ex8.scala
@@ -0,0 +1,5 @@
+package org.cbi.scala_for_the_impatient.unit4
+
+object Ex8 {
+  def minmax(values: Array[Int]) = (values.min, values.max)
+}
---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit4/Ex8Test.scala
@@ -0,0 +1,10 @@
+package org.cbi.scala_for_the_impatient.unit4
+
+import org.specs2.mutable.Specification
+
+object Ex8Test extends Specification {
+
+  "Ex8Test" should {
+    "minmax function on arr(-3, 4, 5, -7, -2, 8) return (-7, 8)" in { Ex8.minmax(Array(-3, 4, 5, -7, -2, 8)) mustEqual (-7, 8) }
+  }
+}
