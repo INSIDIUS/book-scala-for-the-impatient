b7041b085de91fd4c75ac83167c6821a0f07ab53
y.khomenko <insidius89@gmail.com>
2017-04-17 11:01:02 +0300

    - change impletentation of Tree to mutable.TreeMap

--- src/main/scala/org/cbi/scala_for_the_impatient/unit4/Ex5.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit4/Ex5.scala
@@ -1,10 +1,7 @@
 package org.cbi.scala_for_the_impatient.unit4
 
-import java.util
-
 import scala.collection.mutable
 import scala.io.Source
-import scala.collection.JavaConversions.mapAsScalaMap
 
 object Ex5 {
 
@@ -13,7 +10,8 @@ object Ex5 {
     val words = source.getLines().mkString(" ").split(" ")
     source.close()
 
-    val entries: mutable.Map[String, Int] = new util.TreeMap[String, Int]()
+    val entries = mutable.TreeMap[String, Int]()
+
     for (word <- words) entries.put(word, entries.getOrElse(word, 0) + 1)
 
     entries.toMap
