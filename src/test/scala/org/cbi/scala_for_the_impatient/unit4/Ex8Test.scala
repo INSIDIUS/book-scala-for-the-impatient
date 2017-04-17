6485979945d95f0b6c823c1757cc145011946f0e
yykhomenko <yykhomenko@gmail.com>
2017-04-17 12:21:15 +0300

    - added u4 to test stings

--- src/test/scala/org/cbi/scala_for_the_impatient/unit4/Ex8Test.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit4/Ex8Test.scala
@@ -4,7 +4,7 @@ import org.specs2.mutable.Specification
 
 object Ex8Test extends Specification {
 
-  "Ex8Test" should {
+  "U4 Ex8Test" should {
     "minmax function on arr(-3, 4, 5, -7, -2, 8) return (-7, 8)" in { Ex8.minmax(Array(-3, 4, 5, -7, -2, 8)) mustEqual (-7, 8) }
   }
 }
--- src/test/scala/org/cbi/scala_for_the_impatient/unit4/Ex9Test.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit4/Ex9Test.scala
@@ -4,7 +4,7 @@ import org.specs2.mutable.Specification
 
 object Ex9Test extends Specification {
 
-  "Ex9Test" should {
+  "U4 Ex9Test" should {
     "lteqgt function on arr(-3, 4, 5, -6, -2, 6) return (3, 1, 2)" in {
       Ex9.lteqgt(Array(-3, 4, 5, -6, -2, 6), 4) mustEqual (3, 1, 2)}
   }
