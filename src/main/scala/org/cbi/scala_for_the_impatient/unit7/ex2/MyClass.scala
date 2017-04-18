e4501b9fa85088a2fdb09e68785f39af8f134df1
y.khomenko <insidius89@gmail.com>
2017-04-18 22:17:45 +0300

    - added u7 ex2

---
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit7/ex2/MyClass.scala
@@ -0,0 +1,15 @@
+package org.cbi.scala_for_the_impatient.unit7.ex2
+
+package com {
+  package impatient {
+    class MyClass {
+//      collection.Map //not available here
+    }
+  }
+}
+
+package com {
+  package collection {
+
+  }
+}
