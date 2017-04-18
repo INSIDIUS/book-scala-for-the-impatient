52088a8515a67f32b219b413f968b13d91123e28
y.khomenko <insidius89@gmail.com>
2017-04-18 22:11:27 +0300

    - added u7 ex1

---
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit7/ex1_notcomplete/Person.scala
@@ -0,0 +1,13 @@
+package org.cbi.scala_for_the_impatient.unit7.ex1_notcomplete
+
+package com.horstmann.impatient {
+
+  package people {
+
+    class Person {
+//      com.Employee // -- not available here
+    }
+  }
+}
+
+
---
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit7/ex1_notcomplete/Person2.scala
@@ -0,0 +1,13 @@
+package org.cbi.scala_for_the_impatient.unit7.ex1_notcomplete
+
+package com
+package horstmann
+package impatient
+
+package com {
+  object Employee {}
+}
+
+class Person2 {
+  com.Employee
+}
