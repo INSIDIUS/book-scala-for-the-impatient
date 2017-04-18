04d1306b3fa3cf1550b77a9174e1a02cd1550c7a
yykhomenko <yykhomenko@gmail.com>
2017-04-18 22:30:37 +0300

    - refactored U7 Ex1

--- src/main/scala/org/cbi/scala_for_the_impatient/unit7/ex1/Person.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit7/ex1/Person.scala
@@ -1,17 +1,13 @@
 package org.cbi.scala_for_the_impatient.unit7.ex1
 
-package com {
-  package horstmann {
-    package collection{
-      object MyObject {}
-    }
-  }
+package com.horstmann.collection {
+  object MyObject {}
 }
 
 package com.horstmann.impatient {
   package people {
     class Person {
-//      collection.MyObject // -- not available here
+      collection.MyObject // -- not available here
     }
   }
 }
