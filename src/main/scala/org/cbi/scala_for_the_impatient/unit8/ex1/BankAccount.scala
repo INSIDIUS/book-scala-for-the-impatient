439cd253ddcdac6ee96c2186222b5f22aa88d4aa
yykhomenko <yykhomenko@gmail.com>
2017-04-20 17:03:57 +0300

    - added u8 ex1

---
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit8/ex1/BankAccount.scala
@@ -0,0 +1,7 @@
+package org.cbi.scala_for_the_impatient.unit8.ex1
+
+class BankAccount(initialBalance: Double) {
+  private var balance = initialBalance
+  def deposit(amount: Double) = { balance += amount; balance }
+  def withdraw(amount: Double) = { balance -= amount; balance }
+}
