398925c74269f773848b7e4939fa420b71c8c8a7
yykhomenko <yykhomenko@gmail.com>
2017-04-22 14:27:52 +0300

    - refactored U8 Ex3

--- src/main/scala/org/cbi/scala_for_the_impatient/unit8/ex3/Shape.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit8/ex3/Shape.scala
@@ -10,7 +10,4 @@ case class RightTriangle(a: Int, b: Int) extends Shape { override val square = (
 case class Rectangle(a: Int, b: Int) extends Shape { override val square = a * b }
 
 class Square(override val a: Int) extends Rectangle(a, a)
-
-object Square {
-  def apply(a: Int) = new Square(a)
-}
+object Square { def apply(a: Int) = new Square(a) }
