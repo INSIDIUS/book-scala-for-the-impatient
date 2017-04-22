a4182f3b702a1d22240b434f0e288d74b2edfc22
y.khomenko <insidius89@gmail.com>
2017-04-22 16:49:09 +0300

    - added u8 ex7

---
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit8/ex7/Square.scala
@@ -0,0 +1,13 @@
+package org.cbi.scala_for_the_impatient.unit8.ex7
+
+import java.awt.Rectangle
+
+case class Square(_x: Int, _y: Int, _width: Int) extends Rectangle(_x, _y, _width, _width) {
+  def this(width: Int) { this(0, 0, width) }
+  def this() { this(0) }
+}
+
+object Square {
+  def apply(width: Int) = new Square(width)
+  def apply = new Square
+}
---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit8/ex7/SquareTest.scala
@@ -0,0 +1,21 @@
+package org.cbi.scala_for_the_impatient.unit8.ex7
+
+import org.specs2.mutable.Specification
+
+class SquareTest extends Specification {
+
+  "U8 Ex7 SquareTest" should {
+    "square (primary constructor) is equals" in {
+      val square = Square(4, 5, 6)
+      square.x mustEqual 4 and(square.y mustEqual 5) and(square.width mustEqual 6)
+    }
+
+    "square (secondary constructor) is equals" in {
+      Square(7) mustEqual Square(0, 0, 7)
+    }
+
+    "square (third constructor) is equals" in {
+      Square.apply mustEqual Square(0)
+    }
+  }
+}
