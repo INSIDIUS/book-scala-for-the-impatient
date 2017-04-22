0d3c680ce5fa2c8db97abf691e1e95243ce9161e
y.khomenko <insidius89@gmail.com>
2017-04-22 09:48:38 +0300

    - added u8 ex3

---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit8/ex3/RectangleTest.scala
@@ -0,0 +1,15 @@
+package org.cbi.scala_for_the_impatient.unit8.ex3
+
+import org.specs2.mutable.Specification
+
+object RectangleTest extends Specification {
+
+  val shape = Rectangle(3, 4)
+
+  "U8 Ex3 RectangleTest" should {
+    "square is equals" in { shape.square mustEqual 12 }
+    "toString is equals" in {
+      shape.toString mustEqual "I'm " + shape.getClass.getSimpleName + " and my square is: " + shape.square
+    }
+  }
+}
