c66e2efcf343d19c9ba8590637f884d194da6965
y.khomenko <insidius89@gmail.com>
2017-04-23 17:09:18 +0300

    - refactored U8 Ex3

--- src/main/scala/org/cbi/scala_for_the_impatient/unit8/ex3/RightTriangle.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit8/ex3/RightTriangle.scala
@@ -1,3 +1,5 @@
 package org.cbi.scala_for_the_impatient.unit8.ex3
 
-case class RightTriangle(a: Int, b: Int) extends Shape { override val square = (a * b) / 2 }
+case class RightTriangle(a: Int, b: Int) extends Shape {
+  override val square = (a * b) / 2
+}
