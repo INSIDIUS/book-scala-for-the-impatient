469a46e2c52d20d4e9b2153b1c33be89a22924f9
yykhomenko <yykhomenko@gmail.com>
2017-04-23 17:09:18 +0300

    - refactored U8 Ex3

--- src/main/scala/org/cbi/scala_for_the_impatient/unit8/ex3/Rectangle.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit8/ex3/Rectangle.scala
@@ -1,3 +1,5 @@
 package org.cbi.scala_for_the_impatient.unit8.ex3
 
-case class Rectangle(a: Int, b: Int) extends Shape { override val square = a * b }
+case class Rectangle(a: Int, b: Int) extends Shape {
+  override val square = a * b
+}
