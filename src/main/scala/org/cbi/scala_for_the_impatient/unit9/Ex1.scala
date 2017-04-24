a8157871509188f7c57237e10db46916bd7808c0
yykhomenko <yykhomenko@gmail.com>
2017-04-24 21:08:56 +0300

    - refactored U9 Ex1

---
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit9/Ex1.scala
@@ -0,0 +1,17 @@
+package org.cbi.scala_for_the_impatient.unit9
+
+import java.io.{File, PrintWriter}
+
+import scala.io.Source
+
+object Ex1 {
+
+  def reverseFile(file: File) {
+
+    val source = Source.fromFile(file)
+    val revLines = try source.getLines().toList.reverseIterator finally source.close()
+
+    val destination = new PrintWriter(file)
+    try revLines foreach (line => destination.write(line + System.lineSeparator)) finally destination.close()
+  }
+}
---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit9/Ex1Test.scala
@@ -0,0 +1,26 @@
+package org.cbi.scala_for_the_impatient.unit9
+
+import java.io.{File, PrintWriter}
+import java.nio.file.Files
+
+import org.cbi.scala_for_the_impatient.unit9.Ex1.reverseFile
+import org.specs2.mutable.Specification
+
+import scala.io.Source
+
+object Ex1Test extends Specification {
+
+  "U9 Ex1 Test" should {
+    "reverseFile works correctly" in {
+
+      val temp = Files.createTempFile("scala", "test").toFile
+      val out = new PrintWriter(temp)
+      out.write("hello world" + System.lineSeparator + "hello2 world2")
+      out.close()
+
+      reverseFile(temp)
+
+      Source.fromFile(temp).mkString mustEqual "hello2 world2" + System.lineSeparator + "hello world" + System.lineSeparator
+    }
+  }
+}
