af7dbd8e9e5d03e95f9c51c9800e65f6a0ebbb00
y.khomenko <insidius89@gmail.com>
2017-04-26 15:00:50 +0300

    - refactored U9 Ex2

---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit9/Ex2Test.scala
@@ -0,0 +1,31 @@
+package org.cbi.scala_for_the_impatient.unit9
+
+import java.io.PrintWriter
+import java.nio.file.Files
+
+import org.cbi.scala_for_the_impatient.unit9.Ex2.tabToSpaces
+import org.specs2.mutable.Specification
+
+import scala.io.Source
+
+object Ex2Test extends Specification {
+
+  "U9 Ex2 Test" should {
+    "tabToSpaces" in {
+
+      val temp = Files.createTempFile("scala", "test").toFile
+      val out = new PrintWriter(temp)
+      out.write("hello\tworld\twerwer" + System.lineSeparator +
+        "hello2\tworld2werwrr\tsdfsfsfs" + System.lineSeparator +
+        "hellooooqw\tworld2" + System.lineSeparator)
+      out.close()
+
+      tabToSpaces(temp)
+
+      Source.fromFile(temp).getLines.foreach(println)
+      println()
+
+      Source.fromFile(temp).mkString.length mustEqual 96
+    }
+  }
+}
