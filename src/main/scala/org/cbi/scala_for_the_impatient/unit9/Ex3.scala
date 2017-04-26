a9d86faca367512391d6d18a7683c27d095bde0d
y.khomenko <insidius89@gmail.com>
2017-04-26 15:19:47 +0300

    - refactored U9 Ex3

--- src/main/scala/org/cbi/scala_for_the_impatient/unit9/Ex3.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit9/Ex3.scala
@@ -1,11 +1,9 @@
 package org.cbi.scala_for_the_impatient.unit9
 
+import java.io.File
+
+import scala.io.Source
+
 object Ex3 {
-//  try
-//    source.getLines()
-//      .mkString(" ")
-//      .split(" ")
-//      .filter(_.length >= 10)
-//      .foreach(println)
-//  finally source.close()
+  def bigWord(file: File) = Source.fromFile(file).getLines().mkString(" ").split(" ").filter(_.length >= 12)
 }
