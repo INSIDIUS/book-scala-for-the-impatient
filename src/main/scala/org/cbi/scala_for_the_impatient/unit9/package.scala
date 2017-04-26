e474a1a3707754adb8af6165fc011e90b97cf13b
y.khomenko <insidius89@gmail.com>
2017-04-26 21:23:50 +0300

    - refactored package object

--- src/main/scala/org/cbi/scala_for_the_impatient/unit9/package.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit9/package.scala
@@ -5,16 +5,13 @@ import java.io.File
 import scala.io.Source
 
 package object unit9 {
+
   def applyOnFile(temp: File, f: (Source) => Any) = {
-    val source = Source.fromFile(temp)
-    val result = f(source)
-    source.close()
-    result
+    lazy val source = Source.fromFile(temp)
+    applyOnSource(source, f)
   }
 
   def applyOnSource(source: Source, f: (Source) => Any) = {
-    val result = f(source)
-    source.close()
-    result
+    try f(source) finally source.close()
   }
 }
