05c2cd7cf33a5c108fdd5bfe5bca399146c59458
yykhomenko <yykhomenko@gmail.com>
2017-05-06 20:16:34 +0300

    - added U10 Ex1

---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit10/ex2/OrderedPointTest.scala
@@ -0,0 +1,12 @@
+package org.cbi.scala_for_the_impatient.unit10.ex2
+
+import org.specs2.mutable.Specification
+
+class OrderedPointTest extends Specification {
+
+  "U10 Ex2 OrderedPointTest" should {
+    "lower than" in { Some(OrderedPoint(1, 2).compare(OrderedPoint(2, 0))) must beSome((_: Int) < 0) }
+    "equal to" in { Some(OrderedPoint(1, 2).compare(OrderedPoint(1, 2))) must beSome((_: Int) == 0) }
+    "grater than" in { Some(OrderedPoint(1, 4).compare(OrderedPoint(1, 0))) must beSome((_: Int) > 0) }
+  }
+}
