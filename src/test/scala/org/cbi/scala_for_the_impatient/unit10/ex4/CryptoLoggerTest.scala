cdafb8af5db86e7156140fb76373c7897e47ef2a
y.khomenko <insidius89@gmail.com>
2017-05-07 19:21:31 +0300

    - added U10 Ex4

---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit10/ex4/CryptoLoggerTest.scala
@@ -0,0 +1,15 @@
+package org.cbi.scala_for_the_impatient.unit10.ex4
+
+import org.specs2.mutable.Specification
+
+class CryptoLoggerTest extends Specification {
+
+  "U10 Ex4 CryptoLoggerTest" should {
+    "log must work correctly with key=3" in {
+      CryptoLogger().log("ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz") === "DEFGHIJKLMNOPQRSTUVWXYZABC defghijklmnopqrstuvwxyzabc"
+    }
+    "log must work correctly with key=-3" in {
+      CryptoLogger(key = -3).log("ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz") === "XYZABCDEFGHIJKLMNOPQRSTUVW xyzabcdefghijklmnopqrstuvw"
+    }
+  }
+}
