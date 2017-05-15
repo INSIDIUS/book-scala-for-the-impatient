b07ffc5ce706ce6fb84e82c6104a733df19153a5
Yuriy Khomenko <insidius89@gmail.com>
2017-05-15 20:50:24 +0300

    - refactored

--- src/test/scala/org/cbi/scala_for_the_impatient/unit10/ex2/OrderedPointTest.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit10/ex2/OrderedPointTest.scala
@@ -2,7 +2,7 @@ package org.cbi.scala_for_the_impatient.unit10.ex2
 
 import org.specs2.mutable.Specification
 
-class OrderedPointTest extends Specification {
+object OrderedPointTest extends Specification {
 
   "U10 Ex2 OrderedPointTest" should {
     "lower than" in { Some(OrderedPoint(1, 2).compare(OrderedPoint(2, 0))) must beSome((_: Int) < 0) }
--- src/test/scala/org/cbi/scala_for_the_impatient/unit10/ex4/CryptoLoggerTest.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit10/ex4/CryptoLoggerTest.scala
@@ -2,7 +2,7 @@ package org.cbi.scala_for_the_impatient.unit10.ex4
 
 import org.specs2.mutable.Specification
 
-class CryptoLoggerTest extends Specification {
+object CryptoLoggerTest extends Specification {
 
   "U10 Ex4 CryptoLoggerTest" should {
     "log must work correctly with key=3" in {
