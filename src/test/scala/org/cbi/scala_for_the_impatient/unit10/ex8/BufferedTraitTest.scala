fcfb2fb4168b2507040b2f79ffe9df96eeb653f4
yykhomenko <yykhomenko@gmail.com>
2017-05-18 23:05:02 +0300

    - clear

---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit10/ex8/BufferedTraitTest.scala
@@ -0,0 +1,23 @@
+package org.cbi.scala_for_the_impatient.unit10.ex8
+
+import java.io.ByteArrayInputStream
+
+import org.specs2.mutable.Specification
+
+import scala.collection.mutable.ArrayBuffer
+
+object BufferedTraitTest extends Specification {
+
+  val text = "Have the best of both worlds. Construct elegant class hierarchies for maximum code reuse and extensibility, implement their behavior using higher-order functions. Or anything in-between."
+
+  "U10 Ex8 BufferedTraitTest" should {
+    "trait method works correctly" in {
+      val in = new ByteArrayInputStream(text.getBytes()) with Buffered
+
+      val result = ArrayBuffer[Char]()
+      while (in.available() > 0) result += in.read().toChar
+
+      result.mkString("") === text
+    }
+  }
+}
