e2d5b169a910ec73def814c61fad2967e7feeb56
Yuriy Khomenko <insidius89@gmail.com>
2017-05-29 23:07:10 +0300

    - added U11 Ex5

---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit11/ex5/TableTest.scala
@@ -0,0 +1,18 @@
+package org.cbi.scala_for_the_impatient.unit11.ex5
+
+import org.specs2.mutable.Specification
+
+class TableTest extends Specification {
+
+  "U11 Ex5 TableTest" should {
+
+    "toString works correctly" in {
+      (Table() | "Java" | "Scala" || "Gosling" | "Odersky" || "JVM" | "JVM, .NET" toString) ===
+        "<table><tr><td>Java</td><td>Scala</td></tr><tr><td>Gosling</td><td>Odersky</td></tr><tr><td>JVM</td><td>JVM, .NET</td></tr></table>"
+    }
+
+    "toString2 works correctly" in {
+      (Table() | "Java" | "Scala" toString) === "<table><tr><td>Java</td><td>Scala</td></tr></table>"
+    }
+  }
+}
