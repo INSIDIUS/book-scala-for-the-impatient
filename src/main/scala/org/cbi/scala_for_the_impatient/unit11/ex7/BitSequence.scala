9d324c52fa68776dd3c2c34551159f61f486a275
Yuriy Khomenko <insidius89@gmail.com>
2017-05-30 20:18:25 +0300

    - cleared U11 Ex7

--- src/main/scala/org/cbi/scala_for_the_impatient/unit11/ex7/BitSequence.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit11/ex7/BitSequence.scala
@@ -5,15 +5,15 @@ case class BitSequence() {
 
   override def toString: String = s"BitSequence[${bitSet.toBinaryString}]"
 
-  private def checkBorders(index: Byte): Unit =
+  private def checkBorders(index: Byte) =
     if (index < 0 || index > 63) throw new IllegalArgumentException(s"index must be between 0 and 64, actual: $index")
 
-  def apply(index: Byte): Boolean = {
+  def apply(index: Byte) = {
     checkBorders(index)
     ((bitSet & (1 << index)) >> index) == 1
   }
 
-  def update(index: Byte, value: Boolean): Unit = {
+  def update(index: Byte, value: Boolean) {
     checkBorders(index)
     if (value) bitSet |= (1 << index)
     else bitSet &= ~(1 << index)
