cb95c78b57433527d81527cc166bdaf4bc0ceb87
Yuriy Khomenko <insidius89@gmail.com>
2017-06-04 22:00:59 +0300

    - fix test description U12 Ex1

--- src/test/scala/org/cbi/scala_for_the_impatient/unit12/ex1/CollectionsTest.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit12/ex1/CollectionsTest.scala
@@ -7,8 +7,6 @@ class CollectionsTest extends Specification {
   val expected = for (i <- -5 to 5) yield (i, i * i)
 
   "U12 Ex1 CollectionTest" should {
-    "values" in {
-      Collections.values(x => x * x, -5, 5) === expected
-    }
+    "values works correctly" in { Collections.values(x => x * x, -5, 5) === expected }
   }
 }
