09149e46510d8d7aaa4f0284b5b207169be8da26
yykhomenko <yykhomenko@gmail.com>
2017-06-05 18:08:15 +0300

    - fix test description U12 Ex5

--- src/test/scala/org/cbi/scala_for_the_impatient/unit12/ex5/LargestExTest.scala
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit12/ex5/LargestExTest.scala
@@ -4,7 +4,7 @@ import org.specs2.mutable.Specification
 
 class LargestExTest extends Specification {
 
-  "LargestExTest" should {
+  "U12 Ex5 LargestExTest" should {
     "largest works correctly" in { LargestEx.largest(x => 10 * x - x * x, 1 to 10) === 25 }
   }
 }
