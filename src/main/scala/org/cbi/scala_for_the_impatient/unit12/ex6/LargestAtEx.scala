2de1879953b43b65d31a934d28a944e3e7f9e6d3
yykhomenko <yykhomenko@gmail.com>
2017-06-05 18:58:18 +0300

    - added U12 Ex6

--- src/main/scala/org/cbi/scala_for_the_impatient/unit12/ex6/LargestAtEx.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit12/ex6/LargestAtEx.scala
@@ -1,5 +1,5 @@
 package org.cbi.scala_for_the_impatient.unit12.ex6
 
 object LargestAtEx {
-  lar
+  def largestAt(fun: (Int) => Int, inputs: Seq[Int]) = inputs.map(x => (x, fun(x))).maxBy(p => p._2)._1
 }
