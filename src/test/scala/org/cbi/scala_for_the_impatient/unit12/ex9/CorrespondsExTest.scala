c73f55a6065a477934a20c04e28980ec93374262
yykhomenko <yykhomenko@gmail.com>
2017-06-06 19:56:21 +0300

    - added U12 Ex9

---
+++ src/test/scala/org/cbi/scala_for_the_impatient/unit12/ex9/CorrespondsExTest.scala
@@ -0,0 +1,17 @@
+package org.cbi.scala_for_the_impatient.unit12.ex9
+
+import org.specs2.mutable.Specification
+
+class CorrespondsExTest extends Specification {
+
+  val strings = Array("One", "Two", "Three")
+  val lengthsTrue = Array(3, 3, 5)
+  val lengthsFalse = Array(3, 3, 6)
+
+  "U12 Ex9 CorrespondsExTest" should {
+    "corresponds works correctly" in {
+      CorrespondsEx(strings).corresponds(lengthsTrue, _.length == _) === true
+      CorrespondsEx(strings).corresponds(lengthsFalse, _.length == _) === false
+    }
+  }
+}
