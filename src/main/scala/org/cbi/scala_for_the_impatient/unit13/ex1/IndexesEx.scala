ff4d83e34b794f0cdc7a9edf88c032b6b93c045e
Yuriy Khomenko <insidius89@gmail.com>
2017-06-08 23:21:30 +0300

    - added U13 Ex1

---
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit13/ex1/IndexesEx.scala
@@ -0,0 +1,15 @@
+package org.cbi.scala_for_the_impatient.unit13.ex1
+
+import scala.collection.mutable
+import scala.collection.immutable
+
+object IndexesEx {
+
+  implicitly[Ordering[Int]]
+
+  def indexes(string: String) = string
+      .zipWithIndex
+      .foldLeft(immutable.Map[Char, mutable.SortedSet[Int]]())(
+        (m, p) => m + (p._1 -> (m.getOrElse(p._1, mutable.SortedSet.empty) + p._2))
+      )
+}
