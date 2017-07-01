ac0fb9803506e785bfd7334e21dcb8bb6025be37
yurii-khomenko <insidius89@gmail.com>
2017-07-01 19:35:07 +0300

    - corrected U13 ex10

--- src/main/scala/org/cbi/scala_for_the_impatient/unit13/ex10/HarryHacker.scala
+++ src/main/scala/org/cbi/scala_for_the_impatient/unit13/ex10/HarryHacker.scala
@@ -6,8 +6,20 @@ object HarryHacker {
 
   def frequency(string: String): mutable.Map[Char, Int] =
     string
+      .toList
+      .par
       .aggregate(mutable.Map[Char, Int]())(
+
         { (map, ch) => map(ch) = map.getOrElse(ch, 0) + 1; map },
-        { (p1, p2) => p1 ++ p2 }
+        { (p1, p2) => {
+
+            val result = (p1.keySet ++ p2.keySet).map(
+              ch => (ch, p1.getOrElse(ch, 0) + p2.getOrElse(ch, 0))
+            )
+              .toMap[Char, Int]
+
+            mutable.Map(result.toSeq: _*)
+          }
+        }
       )
 }
